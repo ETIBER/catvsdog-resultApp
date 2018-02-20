2f8fa80663042ca022e7318b9ea5c3fcc2c5c84b
ETBE <ebernoux@octo.com>
2018-02-20 15:33:09 +0100

    update no wait for scheduler

--- src/main/java/scheduler/ImportResultSchedulerImpl.java
+++ src/main/java/scheduler/ImportResultSchedulerImpl.java
@@ -34,6 +34,6 @@ public class ImportResultSchedulerImpl implements ImportResultScheduler {
 
     @Override
     public void execute() {
-        scheduler.scheduleAtFixedRate(this, 1, 1, TimeUnit.HOURS);
+        scheduler.scheduleAtFixedRate(this, 0, 1, TimeUnit.HOURS);
     }
 }
